16cc9c4197150436f24ba0775241437c759b78b7
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2025-09-04 12:37:55 +0300

    update

---
+++ Tund1.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpBasics
+{
+    internal class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Näide 0:");
+            Console.WriteLine("Tere tulemast!");
+
+            Example1();
+            Example2();
+        }
+
+        public static void Example1()
+        {
+            Console.WriteLine("Näide 1:");
+
+            Console.WriteLine("Tere tulemast!");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, " + eesnimi);
+        }
+
+        public static void Example2()
+        {
+            Console.WriteLine("Näide 2:");
+
+            Console.WriteLine("Tere tulemast!");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, " + eesnimi);
+            int arv1 = int.Parse(Console.ReadLine());
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+        }
+    }
+}
