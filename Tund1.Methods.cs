4e5a4ac8e0b7cb43b7edb4135543bc3d486411b1
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2025-09-04 13:20:12 +0300

    update

---
+++ Tund1.Methods.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpBasics.Tund1
+{
+    internal class Tund1Methods
+    {
+        public float Calculator(float num1, float num2)
+        {
+            float result = num1 * num2;
+            return result;
+        }
+    }
+}
--- Tund1.cs
+++ Tund1.cs
@@ -1,35 +1,46 @@
-﻿namespace CSharpBasics
+﻿using System.Text;
+
+namespace CSharpBasics.Tund1
 {
     internal class Startclass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Näide 0:");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Random random = new();
+
+            int randomA = random.Next(-100, 200);
+            Console.WriteLine(randomA);
+
+            Tund1Methods methods = new Tund1Methods();
+            Console.WriteLine("Kalkulaatori tulemus: " + methods.Calculator(randomA, 2));
+
+            /*
             Console.WriteLine("Tere tulemast!");
+            Console.Write("Mis on sinu nimi? => ");
 
-            Example1();
-            Example2();
-        }
-
-        public static void Example1()
-        {
-            Console.WriteLine("Näide 1:");
-
-            Console.WriteLine("Tere tulemast!");
             string eesnimi = Console.ReadLine();
-            Console.WriteLine("Tere, " + eesnimi);
-        }
+            Console.WriteLine($"{eesnimi}, Rõõm näha\n");
 
-        public static void Example2()
-        {
-            Console.WriteLine("Näide 2:");
+            int a = 1000;
+            char character = 'A';
 
-            Console.WriteLine("Tere tulemast!");
-            string eesnimi = Console.ReadLine();
-            Console.WriteLine("Tere, " + eesnimi);
-            int arv1 = int.Parse(Console.ReadLine());
-            int arv2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+            Console.Write($"a on {a}, Sisesta b => ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"a + b = {a + b}");
+
+            Console.WriteLine("Ujukomaarv");
+
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+
+            float fl = float.Parse(Console.ReadLine());
+            Console.WriteLine(fl);
+
+            bool b = true;
+            Console.WriteLine(b);*/
         }
     }
 }
